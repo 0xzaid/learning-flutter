a9580cec0bd845f707dddc68c1c0c4f0e8c25df2
zaid <zald0001@student.monash.edu>
2023-07-08 18:05:21 +0300

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -201,6 +201,7 @@ class BigCard extends StatelessWidget {
     final theme = Theme.of(context);
     final style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
     );
 
     return Card(
